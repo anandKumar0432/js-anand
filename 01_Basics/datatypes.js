66cd1078f90a34e2d224d3e05c5902bc6505488d
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2023-02-26 07:06:48 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,21 @@
+ "use strict";  // treat all js code as newer version
+ // alert ( 3O+3) we are using nodejs, not browser
+//  console.log(3 + 3)
+//  //code readability should be high
+//  console.log("anand") 
+
+
+ let name = "anand"
+ let age = 18 
+ let isLoggedIn = false
+
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ // null => standalone value
+ // undefined => 
+ // symbol => unique
+
+ // object
+
+ console.log(typeof "anand")
