fda7d91d00be8838fe707fe223a89c90f8571e3c
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2024-04-27 20:26:33 +0000

    primitive data types

---
+++ 01_Basics/datatypesSummary.js
@@ -0,0 +1,18 @@
+// primitive data types
+// string , number, boolean , null , undefined , symbol, 
+let name = "anand"
+console.log(typeof name)
+ let age = 12
+ console.log(typeof age)
+
+ let isnumber = true
+ console.log(typeof isnumber)
+
+ let str 
+ console.log(typeof str)
+
+ let str1 = Symbol("abc")
+ let str2 = Symbol("abc")
+
+ console.log(str1 === str2)
+ 
