18b7109acdcf0d6a3b9603e4899d786ad9347242
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2024-04-27 22:15:03 +0000

    number and maths

---
+++ 01_Basics/numberAndmaths.js
@@ -0,0 +1,20 @@
+const score = 2000;
+
+// const num = new Number(300)
+// console.log(num);
+
+// console.log(num.toFixed(3))
+// console.log(num.toString().length);
+
+
+
+// Math
+
+// console.log(Math)
+// console.log(Math.round(2.5)) 
+// console.log(Math.abs(-33))
+// console.log(Math.ceil(3.5))
+// console.log(Math.floor(5.6))
+
+console.log(Math.random())
+console.log(Math.floor((Math.random()*10) + 1))
