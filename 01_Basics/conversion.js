afb415cacbb1cadd7005bad5e1ffbb23bba00211
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2024-04-27 19:26:49 +0000

    conversion in js

---
+++ 01_Basics/conversion.js
@@ -0,0 +1,13 @@
+let number = 33
+console.log(number)
+
+let stringnumber = String(number)
+console.log(stringnumber)
+console.log(typeof stringnumber)
+
+
+let IsLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
