8401223081f521e05a2fdee734dd7757535fdb93
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2024-04-27 21:42:50 +0000

    playing with string

---
+++ 01_Basics/string.js
@@ -0,0 +1,28 @@
+let name = "anand"
+
+console.log(name.length)
+
+console.log(name.charAt(3))
+
+let newString1 = "      Aman       "
+console.log(newString1. trim())
+
+const url = "https://anadnkuamr%34saidi"
+console.log(url.replace('%34' , '--'))
+
+console.log(url.includes('anadnk'))
+
+//splitting string into array
+
+const str = "quick fox jumps over the lazy dog"
+
+const words = str.split(' ')
+console.log(words[3]);
+
+// copy of whole string in aray form
+
+const strcopy = str.split()
+console.log(strcopy);
+
+
+
