c800ca44e2509edd9c1e05b1e747404ef1e73be5
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2024-04-28 20:44:23 +0000

    Date and time

---
+++ 01_Basics/dateAndTime.js
@@ -0,0 +1,8 @@
+let newDate = new Date();
+// console.log(newDate.toLocaleTimeString());
+// console.log(newDate.toDateString());
+// console.log(newDate.toLocaleString());
+console.log(newDate.toLocaleString())
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp)
